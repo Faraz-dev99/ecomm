36453baac77909c8745b5dce286caafb03d4c632
faraz-dev99 <arbazuddin242830@gmail.com>
2024-09-26 22:22:54 +0530

    update

--- src/oprations/apiConnect.js
+++ src/oprations/apiConnect.js
@@ -4,7 +4,7 @@ export const axiosmethod=axios.create();
 export const apiConnect=async (method,url,body,headers,params)=>{
     return axiosmethod({
         method:`${method}`,
-        baseURL:`https://ecomm-backend-1.onrender.com/${url}`,
+        baseURL:`https://ecomm-backend-1.onrender.com/api/${url}`,
         data:body?body:null,
         headers:headers?headers:null,
         params:params?params:null
