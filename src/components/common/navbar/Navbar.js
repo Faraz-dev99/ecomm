edc34c071bf647e868a73eea8fbc02f6df6779ea
faraz-dev99 <arbazuddin242830@gmail.com>
2024-09-27 22:16:44 +0530

    update

--- src/components/common/navbar/Navbar.js
+++ src/components/common/navbar/Navbar.js
@@ -67,14 +67,14 @@ const Navbar = () => {
   }
   return (
     <>{/* bg-gradient-to-r from-blue-700 to-sky-500 */}
-    <div className='   relative z-50 bg-slate-950 text-white flex flex-wrap  justify-between py-2 px-2 text-lg font-semibold max-sm:flex-col gap-3'>
+    <div className='   relative z-50 bg-slate-950 text-white flex flex-wrap  justify-between py-2 px-2 pt-4 text-lg font-semibold max-sm:flex-col gap-3'>
       <div className=' flex gap-2'>
       <div className=' md:hidden flex items-center' onClick={()=>menuToggle?SetMenuToggle(false):SetMenuToggle(true)}>{menuToggle?<MenuIcon />:<CloseIcon />}</div>
       <NavLink to={'/'} className='  '><span className=' '>Bazaar</span><span className=' text-sky-500'>Seva</span></NavLink>
       </div>
     
-        <div className=' flex flex-wrap justify-center items-center gap-2  max-md:w-full'>
-         <form className=' flex justify-center items-center px-2 py-2 border max-md:w-full mx-2 rounded-md  border-sky-100 bg-sky-100'>
+        <div className={` flex flex-wrap justify-center items-center gap-2 ${userloggedin?'md:min-w-[500px]':null}   max-md:w-full`}>
+         <form className=' flex justify-center w-full items-center px-2 py-2 border max-md:w-full rounded-md  border-sky-100 bg-sky-100'>
          <button type='submit' className='flex justify-center items-center mr-2'><SearchIcon className=' text-slate-800 font-thin text-xs' style={{fontSize:'18px'}}/></button>
          <input className=' outline-none w-full bg-sky-100 border-none text-black font-normal text-sm placeholder:text-xs placeholder:text-slate-800'
           placeholder='search product..'
@@ -86,20 +86,20 @@ const Navbar = () => {
         
       </div>
       {
-        userloggedin?<div ref={usermenuRef} className=' flex justify-center text-sm items-center relative max-md:absolute max-md:top-2 max-md:right-2' style={{zIndex:'1'}}>
+        userloggedin?<div ref={usermenuRef} className=' flex justify-center text-sm items-center relative max-md:absolute max-md:top-4 max-md:right-2' style={{zIndex:'1'}}>
         <div   className='flex cursor-pointer' onClick={()=>{usermenuToggle?setUsermenuToggle(false):setUsermenuToggle(true)}}>
         <div  className=' cursor-pointer flex justify-center items-center text-white bg-red-600 rounded-full h-7 w-7 text-xl'
           >{userpfpdefault}</div>{/* <ArrowDropDownIcon className=' -ml-1' style={{ fontSize: "30px", fontWeight: 'bold' }} /> */}
         </div>
           
           {
-            usermenuToggle?<div  className=' absolute border border-slate-700 top-10 right-0 flex flex-col  gap-2 font-normal bg-slate-900/95 text-white px-4 py-2 rounded-md'>
+            usermenuToggle?<div  className=' absolute border min-w-[250px] border-slate-700 top-10 right-0 flex flex-col  gap-2 font-normal bg-slate-900/95 text-white px-3 py-2 rounded-md'>
               <div className=' flex flex-col gap-2 mb-2'>
                 <div className=' text-sky-500'>{userloggedin.username}</div>
                 <div className=' w-full overflow-hidden text-ellipsis whitespace-nowrap'>{userloggedin.email}</div>
               </div>
             <NavLink to='dashboard/profile' className='flex items-center gap-2 cursor-pointer py-2 px-4 bg-slate-700 bg-opacity-60 rounded-2xl'><DashboardIcon style={{fontSize:"16px"}}/>Dashboard</NavLink>
-            <div className='flex ml-28 my-2 items-center gap-1 cursor-pointer' onClick={logout}><LogoutIcon style={{fontSize:"16px"}}/>Logout</div>
+            <div className='flex ml-40 my-2 items-center gap-1 cursor-pointer' onClick={logout}><LogoutIcon style={{fontSize:"16px"}}/>Logout</div>
          </div>:null
           }
         </div>:null
