4b8f5e12c0f668ee3c1843fcf061a29c2c2f0ad1
faraz-dev99 <arbazuddin242830@gmail.com>
2024-11-06 16:09:43 +0530

    u

--- src/data/applicationUrls.js
+++ src/data/applicationUrls.js
@@ -1,4 +1,4 @@
-export const applicationBaseUrl="http://localhost:3000";
+export const applicationBaseUrl="https://ecomm-seven-nu.vercel.app";
 
-//https://ecomm-seven-nu.vercel.app/
+//https://ecomm-seven-nu.vercel.app
 //http://localhost:3000
